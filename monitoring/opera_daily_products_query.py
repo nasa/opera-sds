6649bb7f0c6236c3123ee3249cd50851c3ab5384
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2024-07-15 17:01:09 -0600

    Reducing the plot size

--- monitoring/opera_daily_products_query.py
+++ monitoring/opera_daily_products_query.py
@@ -58,6 +58,6 @@ for ic, collection in enumerate(COLLECTIONS):
             color=COLORS[ic], label=f"{LABELS[ic]}")
     plt.legend(loc="upper right")
 
-plt.savefig('opera_daily_products_query.png', bbox_inches='tight', dpi=500)
+plt.savefig('opera_daily_products_query.png', bbox_inches='tight', dpi=400)
 plt.close(fig)
 # plt.show()
