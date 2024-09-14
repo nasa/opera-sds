950dc59a1dfdd2beadbfca8da83bb7bbbc64235a
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2024-09-14 02:19:27 -0600

    Adding DSWx-S1 to monitoring dashboard

--- monitoring/opera_daily_products_query.py
+++ monitoring/opera_daily_products_query.py
@@ -4,21 +4,23 @@ for each product
 """
 
 from datetime import date, timedelta
+from time import gmtime, strftime
 import logging
 
 from cmr import GranuleQuery
 import matplotlib.pyplot as plt
 logging.basicConfig(level=logging.INFO)
 
-NUM_DAYS = 3
-COLLECTIONS = ["HLSL30", "HLSS30", "OPERA_L3_DSWX-HLS_V1", "OPERA_L3_DSWX-HLS_V1",
+NUM_DAYS = 10
+COLLECTIONS = ["HLSL30", "HLSS30", "OPERA_L3_DSWX-HLS_V1",
                "SENTINEL-1A_SLC", "OPERA_L2_RTC-S1_V1", "OPERA_L2_CSLC-S1_V1", "OPERA_L3_DSWX-S1_V1"]
-LABELS = ["HLSL30", "HLSS30", "DSWX-HLS", "DSWX-HLS",
+LABELS = ["HLSL30", "HLSS30", "DSWX-HLS",
           "S1A", "RTC-S1", "CSLC-S1", "DSWX-S1"]
-COLORS = ["greenyellow", "greenyellow", "darkgreen", "darkgreen",
+COLORS = ["greenyellow", "greenyellow", "darkgreen",
           "skyblue", "deepskyblue", "deepskyblue", "deepskyblue"]
 
 today = date.today()
+now = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 dates_list: list[date] = [today - timedelta(days=day) for day in range(NUM_DAYS)]
 dates_list.reverse()
 
@@ -27,12 +29,16 @@ logging.info(dates_list)
 # configure plotting
 plt.rcParams["font.size"] = 4
 plt.rcParams["figure.titlesize"] = 10
-fig, ax = plt.subplots(2,  4)
-fig.suptitle('Number of Products / Day', fontsize=12)
-fig.text(0.5, 0.9, f"{dates_list[0]} - {dates_list[NUM_DAYS-1]}",
+fig = plt.figure()
+fig.suptitle(f'# of Products / Day from: {dates_list[0]} to: {dates_list[NUM_DAYS-1]}',
+             fontsize=12)
+fig.text(0.5, 0.9, f"Updated: {now} (GMT)",
          horizontalalignment="center", fontsize=10)
 
+
 # loop over collections11
+irow = 0
+icol = 0
 for ic, collection in enumerate(COLLECTIONS):
 
     # loop over days
@@ -52,11 +58,17 @@ for ic, collection in enumerate(COLLECTIONS):
         products[i] = num_products
 
     # plot this product
-    plt.subplot(2, 4, ic+1)
+    ax = plt.subplot2grid((2, 4), (irow, icol))
+    # plt.subplot(2, 4, ic+1)
     plt.bar(dates_list, products, width=0.9,
             tick_label=[x.strftime("%d") for x in dates_list],
             color=COLORS[ic], label=f"{LABELS[ic]}")
     plt.legend(loc="upper right")
+    icol += 1
+    if ic == 2:
+        irow += 1
+        icol = 0
+
 
 plt.savefig('opera_daily_products_query.png', bbox_inches='tight', dpi=400)
 plt.close(fig)
